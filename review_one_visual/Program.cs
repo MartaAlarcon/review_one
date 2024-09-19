56a176087ba9726335fbd2deadbe7a5188338927
MartaAlarcon <marta.alarcon.7e7@itb.cat>
2024-09-19 11:29:55 +0200

    Program commit

---
+++ review_one_visual/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    const int MAX = 3;
+    static void Main(string[] args)
+    {
+        int nota1 = Numero(1);
+        int nota2 = Numero(2);
+        int nota3 = Numero(3);
+
+        Console.WriteLine("El promig de les notes es: " + Suma(nota1, nota2, nota3));
+
+        Answer(Suma(nota1, nota2, nota3));
+    }
+    public static void Answer(float answer)
+    {
+        if (answer >= 6)
+        {
+            Console.WriteLine("Aprovat");
+        }
+        else if (answer < 6)
+        {
+            Console.WriteLine("Suspès");
+        }
+    }
+
+    public static int Numero(int number)
+    {
+
+        Console.WriteLine($"Introdueix la {Order(number)} qualificació");
+        int nota = Convert.ToInt32(Console.ReadLine());
+        return nota;
+
+    }
+    public static string Order(int number)
+    {
+        if (number == 1)
+        {
+            return "primera";
+        }
+        else if (number == 2)
+        {
+            return "segona";
+        }
+        else
+        {
+            return "tercera";
+        }
+    }
+    public static float Suma(float a, float b, float c)
+    {
+        return (a + b + c) / MAX;
+    }
+}
